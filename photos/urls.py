b728124af61cf31e3b7d8544e460ecbfd1032fe7
Robert Kirui <kirugik79@gmail.com>
2022-05-28 15:48:20 +0300

    Feat: Configure project settings to upload images

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static  
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('details/<str:pk>/', views.photo_details, name='details'),
     path('upload/', views.upload_photo, name='upload'), 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
