efcb33ef2c020e7168183b5987d733ca84e7b781
Robert Kirui <kirugik79@gmail.com>
2022-05-28 15:58:10 +0300

    Feat: Update index view function to diplay photos

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,51 @@
 from django.shortcuts import render, redirect
+from .models import Category, Location, Image
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    category = request.GET.get('category')
+    if category == None:
+        photos = Image.objects.all()
+    else:
+        photos = Image.objects.filter(category__name=category) 
+
+    categories = Category.objects.all()  
+
+    context = {'categories':categories, 'photos':photos}
+    return render(request, 'index.html', context)
+
+
 
 
 def photo_details(request, pk):
-    return render(request, 'photo_details.html')
+    photo = Image.objects.get(id=pk)
+    return render(request, 'photo_details.html', {'photo':photo}) 
+
+
 
 
 def upload_photo(request):
-    return render(request, 'upload_photo.html')  
+    pass 
+    # categories = Category.objects.all()
+    
+    # if request.method == "POST":
+    #     data = request.POST
+    #     image = request.FILES.get("image") 
+
+    #     if data['category'] != 'none':
+    #         category = Category.objects.get(id=data['category'])
+    #     elif data['category_new'] != '':
+    #         category, created = Category.objects.get_or_create(name=data['category_new']) 
+    #     else:
+    #         category = None
+
+    #     photo = Image.objects.create(
+    #         category = category,
+    #         description = data['description'],
+    #         image = image,
+    #     )
+
+    #     return redirect('index') 
+
+    # context = {'categories':categories}
+    # return render(request, 'upload_photo.html', context)  
