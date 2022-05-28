aa62dc2d6a7aad4e681568928cf0409f05c2730f
Robert Kirui <kirugik79@gmail.com>
2022-05-28 12:34:27 +0300

    Feat: Create index page, photo details, and upload new photo view functions

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def photo_details(request, pk):
+    return render(request, 'photo_details.html')
+
+
+def upload_photo(request):
+    return render(request, 'upload_photo.html')  
